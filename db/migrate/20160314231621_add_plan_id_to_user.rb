e885761cf1134cddade2c09a6229040ef8db30c5
prakashwaghwani <pwaghwani@gmail.com>
2016-03-14 19:17:38 -0400

    added plan_id to users table

---
+++ db/migrate/20160314231621_add_plan_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddPlanIdToUser < ActiveRecord::Migration
+  def change
+    add_reference :users, :plan, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314230721) do
+ActiveRecord::Schema.define(version: 20160314231621) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(version: 20160314230721) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["plan_id"], name: "index_users_on_plan_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

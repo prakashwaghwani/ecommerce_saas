845b02e8fdd3dc2594e154247b6cbf0aa8829d63
prakashwaghwani <pwaghwani@gmail.com>
2016-03-15 00:22:42 -0400

    fixed template?

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,6 @@ class ContactMailer < ActionMailer::Base
     @name = name
     @email = email
     @body = body
-
-    mail(from: email, subject: 'Contact Form Message')
+    mail(from: email, subject: "Contact Form Message from #{@name}")
   end
 end

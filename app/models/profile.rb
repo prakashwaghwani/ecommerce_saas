179ff5af2434d8e994911f838bfd3fe5dab9a94f
prakashwaghwani <pwaghwani@gmail.com>
2016-03-16 19:58:00 -0400

    made improvements to user profile show page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,7 +5,7 @@ class Profile < ActiveRecord::Base
   validates :phone_number, presence: true
   validates :contact_email, presence: true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   belongs_to :user
